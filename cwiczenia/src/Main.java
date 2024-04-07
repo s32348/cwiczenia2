494d0255c2c77fe463d6ddc3f9ec1539f43fedbb
Jacek Kłobukowski <s32348@pjwstk.edu.pl>
2024-04-07 18:26:56 +0200

    Excercise 2

---
+++ cwiczenia/src/Main.java
@@ -0,0 +1,7 @@
+import Student;
+public class Main {
+    public static void main(String[] args) {
+    Student st=new Student();
+
+    }
+}
---
+++ cwiczenia/src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    Fname();
+    public static String Fname() {
+        System.out.println("test");
+
+    }
+}
